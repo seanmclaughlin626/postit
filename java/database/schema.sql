a8c0afd4d79cd9b5b880d90a57bf675f686a1a7d
Sean McLaughlin <sean.charles.mclaughlin@gmail.com>
2023-04-10 10:42:50 -0400

    Set up initial database schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,39 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE forums (
+	forum_id SERIAL,
+	forum_name varchar(50) NOT NULL UNIQUE
+	CONSTRAINT PK_forum PRIMARY KEY (forum_id)
+);
+
+CREATE TABLE posts (
+	post_id SERIAL,
+	forum_id int,
+	author_id int NOT NULL,
+	title varchar(300) NOT NULL,
+	content varchar(40000),
+	image_url varchar (2048),
+	upvotes int DEFAULT 0,
+	downvotes int DEFAULT 0,
+	CONSTRAINT PK_post PRIMARY KEY (post_id),
+	CONSTRAINT FK_post FOREIGN KEY(author_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE comments (
+	comment_id SERIAL,
+	author_id int,
+	content varchar(10000),
+	CONSTRAINT PK_comment PRIMARY KEY (comment_id),
+	CONSTRAINT FK_post FOREIGN KEY (author_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE forum_moderators (
+	forum_id int,
+	mod_id int,
+	CONSTRAINT FK_forum FOREIGN KEY (forum_id) REFERENCES forums(forum_id),
+	CONSTRAINT FK_mod FOREIGN KEY (mod_id) REFERENCES users(user_id),
+	CONSTRAINT PK_mods PRIMARY KEY (forum_id, mod_id)
+)
+
 COMMIT TRANSACTION;
