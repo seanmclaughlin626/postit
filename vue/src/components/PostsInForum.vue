af1e779eb71805c02dae549437ab52635315fd3c
Laken Griffith <laken.griffith@gmail.com>
2023-04-12 16:22:05 -0400

    Started creating component to view posts within a forum.

---
+++ vue/src/components/PostsInForum.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="buttons"><button> Most Recent </button>
+    <button> Most Popular </button>
+    </div>
+    <div>
+      <ul>
+          <li>{{}}</li>
+      </ul>
+  </div>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
