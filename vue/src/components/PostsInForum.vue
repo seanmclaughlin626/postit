5d43b4f7dcd154770ba7b7eff6cff1292a12dc56
Sean McLaughlin <sean.charles.mclaughlin@gmail.com>
2023-04-13 10:43:23 -0400

    Finished toggle to sort by popular or new within a forum

--- vue/src/components/PostsInForum.vue
+++ vue/src/components/PostsInForum.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="buttons">
-      <button v-on:click="displayByPopular = false">Most Recent</button>
       <button v-on:click="displayByPopular = true">Most Popular</button>
+      <button v-on:click="displayByPopular = false">Most Recent</button>
     </div>
     <div v-show="!displayByPopular">
-      <ul v-for="post in $store.state.currentForumPosts" v-bind:key="post.id">
+      <ul v-for="post in recentPosts" v-bind:key="post.id">
         <li>{{post.title}}</li>
       </ul>
     </div>
@@ -24,6 +24,15 @@ export default {
 name: 'posts-in-forum',
 props: {forum: Object},
 computed: {
+
+  recentPosts(){
+          let posts =  this.$store.state.currentForumPosts;
+          return posts.sort((post1, post2) => {
+              let time1 = new Date(post1.timeCreated);
+              let time2 = new Date(post2.timeCreated);
+              return time1.getTime() > time2.getTime() ? -1 : time2.getTime() > time1.getTime() ? 1 : 0;
+          })
+    },
 
   popularPostsByUpvoteScore(){
         let posts =  this.$store.state.currentForumPosts;
