74c475c0ad2f27e2b90db771f3baccb00b451990
Laken Griffith <laken.griffith@gmail.com>
2023-04-13 10:20:41 -0400

    Adding functionality to post in forum to toggle view by popular vs recent.

--- vue/src/components/PostsInForum.vue
+++ vue/src/components/PostsInForum.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <div class="buttons">
-      <button>Most Recent</button>
-      <button>Most Popular</button>
+      <button v-on:click="displayByPopular = false">Most Recent</button>
+      <button v-on:click="displayByPopular = true">Most Popular</button>
     </div>
-    <div>
+    <div v-show="!displayByPopular">
       <ul v-for="post in $store.state.currentForumPosts" v-bind:key="post.id">
+        <li>{{post.title}}</li>
+      </ul>
+    </div>
+    <div v-show="displayByPopular">
+      <ul v-for="post in popularPostsByUpvoteScore" v-bind:key="post.id">
         <li>{{post.title}}</li>
       </ul>
     </div>
@@ -20,11 +25,23 @@ name: 'posts-in-forum',
 props: {forum: Object},
 computed: {
 
+  popularPostsByUpvoteScore(){
+        let posts =  this.$store.state.currentForumPosts;
+        return posts.sort((post1, post2) => {
+            return post1.upvoteScore > post2.upvoteScore ? -1 : post2.upvoteScore > post1.upvoteScore ? 1 : 0;
+        })
+  },
+
 },
 created(){
   postService.getPostsByForumId(this.$route.params.id).then((response) => {
         this.$store.commit("SET_CURRENT_FORUM_POSTS", response.data);
 });
+},
+data() {
+  return {
+  displayByPopular: true
+  }
 }
 }
 </script>
