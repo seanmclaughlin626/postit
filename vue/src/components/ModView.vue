b8e555110d96d1d5b8646e781990cf38108ed639
Sean McLaughlin <sean.charles.mclaughlin@gmail.com>
2023-04-15 12:56:22 -0400

    Added a todo comment lol

--- vue/src/components/ModView.vue
+++ vue/src/components/ModView.vue
@@ -40,7 +40,8 @@ export default {
           this.userToPromote.username = username;
           forumService.promoteUserToMod(this.$route.params.id, this.userToPromote).then((response) => {
               if(response.status == 201){
-                  location.reload();
+                  console.log("Mod promoted!");
+                //   TODO make this a snazzy alert instead of a log
               }
           });
       }
