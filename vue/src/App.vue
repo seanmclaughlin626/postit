a2fdc50713ec35f6295e0b24053f5033ad2caa26
Sean McLaughlin <sean.charles.mclaughlin@gmail.com>
2023-04-15 14:58:00 -0400

    Changed up navbar using bootstrap

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,36 +1,81 @@
 <template>
   <div id="app">
-    <div id="nav" class="nav">
-      <router-link class="nav" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link class="nav" to="/forums">Forums</router-link>
-      <div id="login-stuff" class="nav">
-        <router-link v-bind:to="{name: 'login'}" v-if="$store.state.token == ''" class="nav">Login</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{name: 'register'}" v-if="$store.state.token == ''" class="nav">Sign up</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="nav">Logout</router-link>
-      </div>
-    </div>
-    <router-view />
+    <b-container>
+      <b-navbar toggleable="lg" type="dark" style="background-color: #60233f">
+        <b-navbar-brand href="/">
+          <img
+            class="logo-image"
+            src="./assets/pictures/cropped-logo.png"
+            alt="PostIt"
+          />
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em v-if="$store.state.token !== ''">{{
+                  $store.state.user.username
+                }}</em>
+                <em v-if="$store.state.token === ''">Sign in</em>
+              </template>
+              <b-dropdown-item href="/login" v-show="$store.state.token === ''"
+                >Login</b-dropdown-item
+              >
+              <b-dropdown-item
+                href="/register"
+                v-show="$store.state.token === ''"
+                >Register</b-dropdown-item
+              >
+              <b-dropdown-item href="/logout" v-show="$store.state.token !== ''"
+                >Sign Out</b-dropdown-item
+              >
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
 <style lang="scss">
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
-#nav{
+#nav {
   display: flex;
   justify-content: space-evenly;
   border-bottom: 1px solid black;
 }
-#login-stuff{
+#login-stuff {
   display: flex;
   width: 33%;
   justify-content: flex-end;
 }
-*{
-    background-image: url("./assets/pictures/corkboard2.jpg");
-    
+body {
+  background-image: url("./assets/pictures/corkboard2.jpg");
 }
-.nav{
+.nav {
   background-color: darkgrey;
   background-image: none;
+}
+
+.logo-image {
+  width: 3rem;
+  border: 1px solid #60233f;
+  border-radius: 10%;
+}
+
+b-navbar{
+  background-color: #a46434;
 }
 </style>
